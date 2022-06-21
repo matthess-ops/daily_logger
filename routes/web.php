a38697161865607bd9ad9f55176084a17adbcff6
matthijn <matthijnwur@gmail.com>
2022-06-21 16:03:47 +0200

    full debug

--- routes/web.php
+++ routes/web.php
@@ -35,100 +35,100 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-// ///////////client routes////////////////
+///////////client routes////////////////
 
-// Route::get('/client/daily-report', function () {
-//     return view('client.daily-report');
-// })->name('client.daily-report')->middleware('auth','checkIsClient');
-
-
-// /////////////client activities/////////////////
-
-// // Route::get('/client/activities-logger', function () {
-// //     return view('client.activities.activities-logger');
-// // })->name('client.activities-logger')->middleware('auth','checkIsClient');
-
-// Route::get('/client/activities-logger/edit', 'ActivitiesController@edit'
-// )->name('client.activities-logger')->middleware('auth','checkIsClient');
-
-// Route::get('/client/activities-config', function () {
-//     return view('client.activities.activities-config');
-// })->name('client.activities-config')->middleware('auth','checkIsClient');
+Route::get('/client/daily-report', function () {
+    return view('client.daily-report');
+})->name('client.daily-report')->middleware('auth','checkIsClient');
 
 
-// /////////////////////////
-// Route::get('/client/daily-report-vis', function () {
-//     return view('client.daily-report-vis');
-// })->name('client.daily-report-vis')->middleware('auth','checkIsClient');
+/////////////client activities/////////////////
 
-// Route::get('/client/activities-vis', function () {
-//     return view('client.activities-vis');
-// })->name('client.activities-vis')->middleware('auth','checkIsClient');
+// Route::get('/client/activities-logger', function () {
+//     return view('client.activities.activities-logger');
+// })->name('client.activities-logger')->middleware('auth','checkIsClient');
 
-// //////////////
-// Route::get('/client/account-personal/{id}', 'ClientController@show'
-// )->name('client.account-personal')->middleware('auth','checkIsClient');
+Route::get('/client/activities-logger/edit', 'ActivitiesController@edit'
+)->name('client.activities-logger')->middleware('auth','checkIsClient');
 
-// Route::get('/client/account-personal/{id}/edit', 'ClientController@edit'
-// )->name('client.edit-personal-information')->middleware('auth','checkIsClient');
-
-// Route::put('/client/account-personal/{id}', 'ClientController@update'
-// )->name('client.update-personal-information')->middleware('auth','checkIsClient');
-// ///////////////////
-
-// Route::get('/client/account-password/{id}/edit', 'PasswordController@edit'
-// )->name('client.edit-account-password')->middleware('auth','checkIsClient');
-
-// Route::put('/client/account-password/{id}', 'PasswordController@update'
-// )->name('client.update-password')->middleware('auth','checkIsClient');
-
-// // client.update-password
-
-// // Route::get('/client/account-password', 'ClientController@account'
-// // )->name('client.account-password')->middleware('auth','checkIsClient');
-
-// // Route::post('client/account/save','ClientController@save')->name('client.account.save');
-// // ->middleware('auth','checkIsClient');
+Route::get('/client/activities-config', function () {
+    return view('client.activities.activities-config');
+})->name('client.activities-config')->middleware('auth','checkIsClient');
 
 
+/////////////////////////
+Route::get('/client/daily-report-vis', function () {
+    return view('client.daily-report-vis');
+})->name('client.daily-report-vis')->middleware('auth','checkIsClient');
 
-// //////////////////mentor routes //////////////////
+Route::get('/client/activities-vis', function () {
+    return view('client.activities-vis');
+})->name('client.activities-vis')->middleware('auth','checkIsClient');
+
+//////////////
+Route::get('/client/account-personal/{id}', 'ClientController@show'
+)->name('client.account-personal')->middleware('auth','checkIsClient');
+
+Route::get('/client/account-personal/{id}/edit', 'ClientController@edit'
+)->name('client.edit-personal-information')->middleware('auth','checkIsClient');
+
+Route::put('/client/account-personal/{id}', 'ClientController@update'
+)->name('client.update-personal-information')->middleware('auth','checkIsClient');
+///////////////////
+
+Route::get('/client/account-password/{id}/edit', 'PasswordController@edit'
+)->name('client.edit-account-password')->middleware('auth','checkIsClient');
+
+Route::put('/client/account-password/{id}', 'PasswordController@update'
+)->name('client.update-password')->middleware('auth','checkIsClient');
+
+// client.update-password
+
+// Route::get('/client/account-password', 'ClientController@account'
+// )->name('client.account-password')->middleware('auth','checkIsClient');
+
+// Route::post('client/account/save','ClientController@save')->name('client.account.save');
+// ->middleware('auth','checkIsClient');
 
 
 
-// Route::get('/mentor/account', function () {
-//     return view('mentor.account');
-// })->name('mentor.account')->middleware('auth','checkIsMentor');
+//////////////////mentor routes //////////////////
 
-// Route::get('/mentor/daily-reports', function () {
-//     return view('mentor.daily-reports');
-// })->name('mentor.daily-reports')->middleware('auth','checkIsMentor');
 
-// Route::get('/mentor/clients', function () {
-//     return view('mentor.clients');
-// })->name('mentor.clients')->middleware('auth','checkIsMentor');
 
-// /////////////////admin routes //////////////////
+Route::get('/mentor/account', function () {
+    return view('mentor.account');
+})->name('mentor.account')->middleware('auth','checkIsMentor');
 
-// Route::get('/admin/clients', function () {
-//     return view('admin.clients');
-// })->name('admin.clients')->middleware('auth','checkIsAdmin');
+Route::get('/mentor/daily-reports', function () {
+    return view('mentor.daily-reports');
+})->name('mentor.daily-reports')->middleware('auth','checkIsMentor');
 
-// Route::get('/admin/mentors', function () {
-//     return view('admin.mentors');
-// })->name('admin.mentors')->middleware('auth','checkIsAdmin');
+Route::get('/mentor/clients', function () {
+    return view('mentor.clients');
+})->name('mentor.clients')->middleware('auth','checkIsMentor');
 
-// Route::get('/admin/daily-report-config', function () {
-//     return view('admin.daily-report-config');
-// })->name('admin.daily-report-config')->middleware('auth','checkIsAdmin');
+/////////////////admin routes //////////////////
 
-// Route::get('/admin/create-client', function () {
-//     return view('admin.create-client');
-// })->name('admin.create-client')->middleware('auth','checkIsAdmin');
+Route::get('/admin/clients', function () {
+    return view('admin.clients');
+})->name('admin.clients')->middleware('auth','checkIsAdmin');
 
-// Route::get('/admin/create-mentor', function () {
-//     return view('admin.create-mentor');
-// })->name('admin.create-mentor')->middleware('auth','checkIsAdmin');
+Route::get('/admin/mentors', function () {
+    return view('admin.mentors');
+})->name('admin.mentors')->middleware('auth','checkIsAdmin');
+
+Route::get('/admin/daily-report-config', function () {
+    return view('admin.daily-report-config');
+})->name('admin.daily-report-config')->middleware('auth','checkIsAdmin');
+
+Route::get('/admin/create-client', function () {
+    return view('admin.create-client');
+})->name('admin.create-client')->middleware('auth','checkIsAdmin');
+
+Route::get('/admin/create-mentor', function () {
+    return view('admin.create-mentor');
+})->name('admin.create-mentor')->middleware('auth','checkIsAdmin');
 
 
 // Verb          Path                        Action  Route Name
